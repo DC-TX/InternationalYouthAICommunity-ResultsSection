013bfd95df9f32c78d8fc62fbd424776b1ae07fc
DC-TX <Jb14514@163.com>
2026-06-15 19:16:39 +0800

    Wrap app with Firebase auth provider

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import "./staticQuality.css";
 import App from "./App";
+import { AuthProvider } from "./context/AuthContext";
 
 // React 入口文件
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <App />
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </React.StrictMode>
 );
