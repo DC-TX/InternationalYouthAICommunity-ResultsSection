18ebe5f00339a10bde6b6ee2087c82fbd0dc0fcf
DC-TX <Jb14514@163.com>
2026-06-23 21:14:26 +0800

    Import academy extra motion styles

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import "./staticQuality.css";
+import "./academyExtras.css";
 import App from "./App";
 import { AuthProvider } from "./context/AuthContext";
 
